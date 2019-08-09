8977c5ab086f1b37baae42e3382dddb234e182a3
branchard <benoit.branchard@gmail.com>
2019-08-09 15:18:10 +0200

    add entrypoint

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import dotenv from 'dotenv';
+import Bot from './Bot';
+import {Client, TextChannel} from "discord.js";
+import readline from "readline";
+import UiFacesProvider from "./providers/UiFacesProvider";
+import LolProvider from "./providers/LolProvider";
+
+dotenv.config();
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+const client: Client = new Client;
+
+client.on('ready', () => {
+    const bot = new Bot(client, [new UiFacesProvider(process.env.UIFACES_API_KEY, 'female')]);
+
+    rl.on('line', (line: string) => {
+        for (const [, guild] of client.guilds) {
+            try {
+                for (const [, channel] of guild.channels) {
+                    if (channel instanceof TextChannel) {
+                        channel.send(line);
+                        break;
+                    }
+                }
+            } catch (e) {
+                console.error(`Cannot send line to Guild ${guild.name}`, e);
+            }
+        }
+    });
+
+    client.user.setStatus('online');
+
+    bot.run();
+
+    console.log('Client started...');
+});
+
+client.on('error', (err) => {
+    console.error('Client error:', err);
+});
+
+client.on('disconnect', () => {
+    console.warn('Client disconnect');
+});
+
+client.on('reconnecting', () => {
+    console.warn('Client reconnecting');
+});
+
+client.login(process.env.DISCORD_LOGIN_TOKEN)
+    .then((...args) => {
+        console.log('Client logged', ...args)
+    })
+    .catch((...args) => {
+        console.error('Unable to log the client', ...args)
+    });
+
+process.on('SIGINT', async () => {
+    console.log("Caught interrupt signal");
+    let err: boolean = false;
+    try {
+        await client.user.setStatus("invisible");
+    } catch (e) {
+        err = true;
+        console.error(e);
+    }
+
+    try {
+        await client.destroy();
+    } catch (e) {
+        err = true;
+        console.error(e);
+    }
+
+    console.log('Process exit:', Number(err));
+
+    process.exit(Number(err));
+});
